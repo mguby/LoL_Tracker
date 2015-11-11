f1741b9a305bd44dc574ea21b45d19fbfcd16ed4
Mark <mguby>
2015-11-10 18:33:49 -0600

    test to show that nalcs teams are displayed correctly

--- app/src/androidTest/java/com/markgubatan/loltracker/MainActivityTest2.java
+++ app/src/androidTest/java/com/markgubatan/loltracker/MainActivityTest2.java
@@ -1,14 +1,24 @@
 package com.markgubatan.loltracker;
 
+import android.content.res.Resources;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import com.markgubatan.loltracker.ui.activities.MainActivity;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Espresso version of MainActivity tests.
@@ -35,6 +45,32 @@ public class MainActivityTest2 extends ActivityInstrumentationTestCase2<MainActi
     @SmallTest
     public void testLeaguesFragment_displayed() {
         onView(withId(R.id.main_container)).check(matches(isDisplayed()));
+
+        Resources res = mainActivity.getResources();
+        String[] naLCS = res.getStringArray(R.array.nalcs);
+
+        for(String team : naLCS) {
+            onData(allOf(is(instanceOf(String.class)), withTeamName(team), isDisplayed()))
+                    .inAdapterView(withId(android.R.id.list));
+        }
+    }
+
+    public static Matcher<Object> withTeamName(String name) {
+        return withTeamName(equalTo(name));
+    }
+
+    public static Matcher<Object> withTeamName(final Matcher<String> name) {
+        return new BoundedMatcher<Object, String>(String.class) {
+            @Override
+            protected boolean matchesSafely(String s) {
+                return name.matches(s);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                name.describeTo(description);
+            }
+        };
     }
 
 }
