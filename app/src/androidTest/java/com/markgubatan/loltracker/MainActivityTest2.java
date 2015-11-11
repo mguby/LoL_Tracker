248fe8d405be3f67fce8ec172070412cb7ebdace
mguby <mguby814@gmail.com>
2015-11-11 00:48:01 -0600

    fully functional tests for verifying lists of teams and players

--- app/src/androidTest/java/com/markgubatan/loltracker/MainActivityTest2.java
+++ app/src/androidTest/java/com/markgubatan/loltracker/MainActivityTest2.java
@@ -5,6 +5,7 @@ import android.support.test.espresso.matcher.BoundedMatcher;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.test.suitebuilder.annotation.SmallTest;
+import android.util.Log;
 
 import com.markgubatan.loltracker.ui.activities.MainActivity;
 
@@ -13,6 +14,9 @@ import org.hamcrest.Matcher;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -26,7 +30,9 @@ import static org.hamcrest.Matchers.is;
  */
 public class MainActivityTest2 extends ActivityInstrumentationTestCase2<MainActivity> {
 
+    private static final String TAG = "MainActivityTest2";
     private MainActivity mainActivity;
+    private Resources res;
     private String[] naLCS;
     private String[] euLCS;
     private String[] lpl;
@@ -41,7 +47,7 @@ public class MainActivityTest2 extends ActivityInstrumentationTestCase2<MainActi
     public void setUp() throws Exception {
         super.setUp();
         mainActivity = getActivity();
-        Resources res = mainActivity.getResources();
+        res = mainActivity.getResources();
         naLCS = res.getStringArray(R.array.nalcs);
         euLCS = res.getStringArray(R.array.eulcs);
         lpl = res.getStringArray(R.array.lpl);
@@ -58,24 +64,71 @@ public class MainActivityTest2 extends ActivityInstrumentationTestCase2<MainActi
     public void testLeaguesFragment_displayed() {
         onView(withId(R.id.main_container)).check(matches(isDisplayed()));
         checkTeams(naLCS);
+        onView(withId(R.id.viewpager)).perform(swipeLeft());
         checkTeams(euLCS);
-        checkTeams(lpl);
+        onView(withId(R.id.viewpager)).perform(swipeLeft());
         checkTeams(lck);
+        onView(withId(R.id.viewpager)).perform(swipeLeft());
+        checkTeams(lpl);
+        onView(withId(R.id.viewpager)).perform(swipeLeft());
         checkTeams(lms);
     }
 
     private void checkTeams(String[] teams) {
         for(String team : teams) {
-            onData(allOf( is(instanceOf(String.class)), withTeamName(team), isDisplayed() ))
-                    .inAdapterView(withId(android.R.id.list));
+            onData(allOf( is(instanceOf(String.class)), withName(team) ))
+                    .inAdapterView(allOf(withId(R.id.team_list), isDisplayed()))
+                    .check(matches(isDisplayed()));
         }
     }
 
-    public static Matcher<Object> withTeamName(String name) {
-        return withTeamName(equalTo(name));
+    @MediumTest
+    public void testTeamsFragment_teamLineups() {
+        onView(withId(R.id.main_container)).check(matches(isDisplayed()));
+        checkLineups(naLCS);
+//        onView(withId(R.id.viewpager)).perform(swipeLeft());
+//        checkLineups(euLCS);
+//        onView(withId(R.id.viewpager)).perform(swipeLeft());
+//        checkLineups(lck);
+//        onView(withId(R.id.viewpager)).perform(swipeLeft());
+//        checkLineups(lpl);
+//        onView(withId(R.id.viewpager)).perform(swipeLeft());
+//        checkLineups(lms);
     }
 
-    public static Matcher<Object> withTeamName(final Matcher<String> name) {
+    private void checkLineups(String[] teams) {
+        for(String team : teams) {
+            Log.v(TAG, team);
+            onData(allOf(is(instanceOf(String.class)), withName(team)))
+                    .inAdapterView(allOf(withId(R.id.team_list), isDisplayed()))
+                    .check(matches(isDisplayed()))
+                    .perform(click());
+
+            String[] players = getPlayers(team);
+            checkPlayers(players);
+            pressBack();
+        }
+    }
+
+    private String[] getPlayers(String team) {
+        String teamReplace = team.replace(' ', '_').toLowerCase();
+        int id = res.getIdentifier(teamReplace, "array", mainActivity.getPackageName());
+        return res.getStringArray(id);
+    }
+
+    private void checkPlayers(String[] players) {
+        for(String player : players) {
+            onData(allOf(is(instanceOf(String.class)), withName(player) ))
+                    .inAdapterView(withId(R.id.org_list))
+                    .check(matches(isDisplayed()));
+        }
+    }
+
+    public static Matcher<Object> withName(String name) {
+        return withName(equalTo(name));
+    }
+
+    public static Matcher<Object> withName(final Matcher<String> name) {
         return new BoundedMatcher<Object, String>(String.class) {
             @Override
             protected boolean matchesSafely(String s) {
